5b9b54f72ecbe0edc5cf6d2f3130586da0c83077
Valentin <Valentin@192.168.1.48>
2018-10-01 14:49:37 +0200

    Gestion erreur d'axe pour le tri

--- src/chargement/Launcher.java
+++ src/chargement/Launcher.java
@@ -2,6 +2,8 @@ package chargement;
 
 import java.io.IOException;
 
+import exception.NotAnAxisException;
+
 /**
  * Classe principale lançant l'application.
  * @author Valentin
@@ -22,7 +24,12 @@ public class Launcher {
 		for(int i=0;i<faces.length;++i) {
 			faces[i].setCentre_gravite(faces[i].calculCentreGravite());
 		}
-		trierFaces(faces,2);
+		try {
+			trierFaces(faces,3);
+		} catch (NotAnAxisException e) {
+			// TODO Auto-generated catch block
+			System.exit(1);
+		}
 		for(int i=0;i<faces.length;++i) {
 			System.out.println(faces[i]);
 		}
@@ -32,18 +39,22 @@ public class Launcher {
 	 * Trie les faces de la plus éloignée à la plus éloignée en fonction de l'axe depuis lequel on visualise la figure.
 	 * @param axe: 0 pour l'axe x, 1 pour l'axe y et 2 pour l'axe z.
 	 */
-	public static void trierFaces(Face[] faces,int axe){ //TRI A BULLE PEU EFFICACE, IMPLEMENTER UN ALGORITHME DE TRI PLUS PERFORMANT.
-		boolean trie;
-		do {
-			trie=true;
-			for(int i=0;i<faces.length-1;++i) {
-				if(faces[i].getCentre_gravite().getCoordonnees()[axe]>faces[i+1].getCentre_gravite().getCoordonnees()[axe]) {
-					trie=false;
-					Face tmp = faces[i];
-					faces[i] = faces[i+1];
-					faces[i+1] = tmp;
+	public static void trierFaces(Face[] faces,int axe) throws NotAnAxisException{ //TRI A BULLE PEU EFFICACE, IMPLEMENTER UN ALGORITHME DE TRI PLUS PERFORMANT.
+		if(axe<0 || axe >2)
+			throw new NotAnAxisException();
+		else {
+			boolean trie;
+			do {
+				trie=true;
+				for(int i=0;i<faces.length-1;++i) {
+					if(faces[i].getCentre_gravite().getCoordonnees()[axe]>faces[i+1].getCentre_gravite().getCoordonnees()[axe]) {
+						trie=false;
+						Face tmp = faces[i];
+						faces[i] = faces[i+1];
+						faces[i+1] = tmp;
+					}
 				}
-			}
-		}while(!trie);
+			}while(!trie);
+		}
 	}
 }
