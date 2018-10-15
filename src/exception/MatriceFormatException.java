319b57444e376499bd5ebf0743b5245d69dd05b5
Valentin <Valentin@192.168.1.48>
2018-10-15 23:10:25 +0200

    a

--- src/exception/MatriceFormatException.java
+++ src/exception/MatriceFormatException.java
@@ -1,9 +1,11 @@
 package exception;
 
+import javax.swing.JOptionPane;
+
 @SuppressWarnings("serial")
 public class MatriceFormatException extends Exception{
 	
 	public MatriceFormatException() {
-		System.out.println("La multiplication de deux matrices n'est possible que si le nombre de colonnes de la première est égal au nombre de lignes de la seconde.");
+		JOptionPane.showMessageDialog(null,"La multiplication de deux matrices n'est possible que si le nombre de colonnes de la première est égal au nombre de lignes de la seconde.","Erreur Matrice Format",JOptionPane.ERROR_MESSAGE);
 	}
 }
